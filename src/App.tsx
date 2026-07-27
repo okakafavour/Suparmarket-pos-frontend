aa140dd625cc551f32bf684d5fd08932a3723b2b
okaka favour <okakafavour81@gmail.com>
2026-07-27 12:27:01 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        TheftGuard POS
+      </h1>
+    </div>
+  );
+}
